2d841a015ee4fc982f3938c41218a6fbfc51344e
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-11-16 20:25:47 +0530

    Update whatsapp.js

--- plugins/whatsapp.js
+++ plugins/whatsapp.js
@@ -1,8 +1,11 @@
 /*------------------------------------------------------------------------------------------------------------------------------------------------------
 
+
 Copyright (C) 2023 Loki - Xer.
-Licensed under the GPL-3.0 License; you may not use this file except in compliance with the License.
-Jarvis - Loki-Xer
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Jarvis - Loki-Xer 
+
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
