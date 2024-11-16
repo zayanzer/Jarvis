c03ac075a3e0b5e745ee10623e446132b2332bbc
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-11-17 00:16:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const { client, config } = require("./lib/");
 
+const databaseSync = async () => {
+  try {
+    await DATABASE.authenticate({ retry: { max: 3 } });
+    await config.DATABASE.sync();
+  } catch (error) {
+    console.log(`Unable to connect to the database\nerror: ${error.message}`);
+    process.exit(1);
+  }
+};
+
 const start = async () => {
     try {
-        await config.DATABASE.sync();
+        await databaseSync();
         const Client = new client();
         Client.log("starting client...");
         await Client.startServer();
