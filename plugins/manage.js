a41500c5a06281484106196c4c63a3ae1d4d623e
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-11-16 23:12:27 +0530

    Update manage.js

--- plugins/manage.js
+++ plugins/manage.js
@@ -217,6 +217,64 @@ System({
 });
 
 System({
+    pattern: 'welcome ?(.*)',
+    type: 'greetings',
+    fromMe: true,
+    onlyGroup: true,
+    desc: 'set welcome message'
+}, async (message, match) => {
+    const { welcome } = await getData(message.from);
+    if (match.toLowerCase() === 'get') {
+        if (!welcome || !welcome.message) return await message.send('*_Not Set Yet_*');
+        return await message.send(welcome.message);
+    } else if (match.toLowerCase() === 'off') {
+        const status = welcome && welcome.status ? welcome.status : 'false';
+        if (status === 'false') return await message.send(`_already deactivated_`);
+        await setData(message.jid, welcome.message, 'false', 'welcome');
+        return await message.send('*successfully deactivated*');
+    } else if (match.toLowerCase() === 'on') {
+        const status = welcome && welcome.status ? welcome.status : 'false';
+        if (status === 'true') return await message.send(`_already activated_`);
+        await setData(message.jid, welcome.message, 'true', 'welcome');
+        return await message.send('*successfully activated*');
+    } else if (match) {
+        const status = welcome && welcome.status ? welcome.status : 'true';
+        await setData(message.jid, match, status, 'welcome');
+        return await message.send('*successfully set*');
+    }
+    return await message.reply('_*welcome get*_\n_*welcome* thank you for joining &mention_\n*_welcome false_*');
+});
+
+System({
+    pattern: 'goodbye ?(.*)',
+    type: 'greetings',
+    fromMe: true,
+    onlyGroup: true,
+    desc: 'set goodbye message'
+}, async (message, match) => {
+    const { exit } = await getData(message.jid);
+    if (match.toLowerCase() === 'get') {
+        if (!exit || !exit.message) return await message.send('*_Not Set Yet_*');
+        return await message.send(exit.message);
+    } else if (match.toLowerCase() === 'off') {
+        const status = exit && exit.status ? exit.status : 'false';
+        if (status === 'false') return await message.send(`_already deactivated_`);
+        await setData(message.jid, exit.message, 'false', 'exit');
+        return await message.send('*successfully deactivated*');
+    } else if (match.toLowerCase() === 'on') {
+        const status = exit && exit.status ? exit.status : 'false';
+        if (status === 'true') return await message.send(`_already activated_`);
+        await setData(message.jid, exit.message, 'true', 'exit');
+        return await message.send('*successfully activated*');
+    } else if (match) {
+        const status = exit && exit.status ? exit.status : 'true';
+        await setData(message.jid, match, status, 'exit');
+        return await message.send('*successfully set*');
+    }
+    return await message.reply('_*goodbye get*_\n_*goodbye* thank you for joining &mention_\n*_goodbye false_*');
+});
+
+System({
     pattern: "antidelete",
     fromMe: true,
     type: "manage",
