a8a5254509e3b3dab4505c5250509d8379d8e4da
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-11-16 20:09:49 +0530

    Update support.js

--- plugins/support.js
+++ plugins/support.js
@@ -32,8 +32,8 @@ System({
     desc: "To get all plugin of jarvis-md",
     type: "support"
 }, async (message) => {
-    const allPluginsData = await getJson('https://api-loki-ser-1o2h.onrender.com/api/jarvis/allplugin');
-    const externalPluginsData = await getJson('https://api-loki-ser-1o2h.onrender.com/api/jarvis/plugin');
+    const allPluginsData = await getJson(api + 'bot/plugin?query=allplugin');
+    const externalPluginsData = await getJson(api + 'bot/plugin?query=pluginlist');
     const formatPluginData = (pluginData) => {
         return Object.entries(pluginData).map(([key, value]) => `*${key}:* ${value.url}`).join('\n\n');
     };
