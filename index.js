94ec86e42fa574c0098a217bb007405bfc37ba02
Ronak sharma <112383311+Ronak-xd@users.noreply.github.com>
2024-11-17 08:37:47 +0530

    fix

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 const { client, config } = require("./lib/");
-const { client, config } = require("./lib/");
 
-
-const databaseSync = async () => {
-  try {
-    await DATABASE.authenticate({ retry: { max: 3 } });
-    await config.DATABASE.sync();
-  } catch (error) {
-    console.log(`Unable to connect to the database\nerror: ${error.message}`);
-    process.exit(1);
-  }
-};
 const start = async () => {
-const start = async () => {
-    try {
     try {
         await config.DATABASE.sync();
-        await databaseSync();
         const Client = new client();
-        const Client = new client();
-        Client.log("starting client...");
         Client.log("starting client...");
         await Client.startServer();
