3ffaf99003cdea1f5ede8fe2a0a8c57bff061d6e
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-11-23 13:29:47 +0530

    Update tool.js

--- plugins/tool.js
+++ plugins/tool.js
@@ -30,14 +30,14 @@ System({
 });
 
 System({
-    pattern: "vv",
-    fromMe: true,
-    type: "tool",
-    alias: ['view'],
-    desc: "get view ones message"
+      pattern: "vv",
+      fromMe: true,
+      type: "tool",
+      alias: ['view'],
+      desc: "get view ones message"
 }, async (message) => {
-   if (!message.reply_message.viewones) return await message.reply("_*Reply to a view once*_");
-   return await message.client.forwardMessage(message.chat, message.reply_message.message, { readViewOnce: true });
+     if (!message.reply_message.viewones) return await message.reply("_*Reply to a view once*_");
+     return await message.client.forwardMessage(message.chat, message.reply_message.message, { readViewOnce: true });
 });
 
 System({
