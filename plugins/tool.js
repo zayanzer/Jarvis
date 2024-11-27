966b9c1ee192d49ddd2bb6dbb2e9cd64e4965763
Ronak sharma <112383311+Ronak-xd@users.noreply.github.com>
2024-11-27 19:13:21 +0530

    fix

--- plugins/tool.js
+++ plugins/tool.js
@@ -21,7 +21,8 @@ System({
 	fromMe: isPrivate,
 	type: "tool",
 	alias: ['pong'],
-	desc: "To check ping"
+	desc: "To check ping",
+	adminAccess: true,
 }, async (message) => {
 	const start = new Date().getTime();
 	const ping = await message.send("*𝆺𝅥 running 𝆺𝅥*");
@@ -30,14 +31,14 @@ System({
 });
 
 System({
-      pattern: "vv",
-      fromMe: true,
-      type: "tool",
-      alias: ['view'],
-      desc: "get view ones message"
+    pattern: "vv",
+    fromMe: true,
+    type: "tool",
+    alias: ['view'],
+    desc: "get view ones message"
 }, async (message) => {
-     if (!message.reply_message.viewones) return await message.reply("_*Reply to a view once*_");
-     return await message.client.forwardMessage(message.chat, message.reply_message.message, { readViewOnce: true });
+   if (!message.reply_message.viewones) return await message.reply("_*Reply to a view once*_");
+   return await message.client.forwardMessage(message.chat, message.reply_message.message, { readViewOnce: true });
 });
 
 System({
