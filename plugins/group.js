911d2105720d54b280159444b745e35cd02ce3e2
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-12-23 14:16:26 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -209,7 +209,7 @@ System({
         match = match || message.reply_message.text;
         if (!match) return await message.reply('*Example :* \n_*tag all*_\n_*tag admin*_\n_*tag text*_\n_*Reply to a message*_');
         await message.send(match, { mentions: participants.map(a => a.id) });
-    } else if (message.reply_message.i) {
+    } else if (message.quoted) {
         return await message.client.forwardMessage(message.jid, message.reply_message.message, { contextInfo: { mentionedJid: participants.map(a => a.id) } });
     } else {
         return await message.reply("*Example :* \n_*tag all*_\n_*tag admin*_\n_*tag me*_\n_*tag text*_\n_*Reply to a message to tag that message*_");
